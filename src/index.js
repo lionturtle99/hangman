faad8bcbe1bdb149927833fe02e3fafdcf374804
Liam Eller <liamthelastson@gmail.com>
2022-02-08 11:30:45 -0800

    Add jquery to hide dshow divs
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DinoIpsum from './js/business.js';
+
+// UI Logic
+
+$("#dino-button").on("click", function() {
+  $("#rules").addClass("hidden");
+  $("#dino-button").addClass("hidden");
+  $("#play-area").removeClass("hidden");
+  let promise = DinoIpsum.makeCall(1, 1);
+  promise.then(function(response) {
+    const dino = JSON.parse(response);
+    $("#dino-name").text(`This the dino ipsum text: ${dino[0][0]}`);
+  });
+});
